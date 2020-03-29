1ade3814c02c0a830a8efe0578cac0f410464965
sridhar-afk <60507327+sridhar-afk@users.noreply.github.com>
2020-03-29 16:29:54 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,19 @@
+package Numbers;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int fact = 1;
+		int num = 5;
+		
+		for(int i=1; i<=num; i++)
+		{
+			fact = fact * i;
+		}
+		System.out.println(fact);
+		
+	}
+
+}
